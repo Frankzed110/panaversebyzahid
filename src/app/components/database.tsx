2cb8542b1ae5402b2343fc5509d0a363a2bd96f8
Syed Zahid <115338908+Frankzed110@users.noreply.github.com>
2023-03-25 17:33:12 +0500

    Syllabus Page Updated

--- src/app/components/database.tsx
+++ src/app/components/database.tsx
@@ -1,35 +1,114 @@
-export const data= [
-{
-    id:1,
-    src: '/Ziasir.webp',
-    heading:'Zia Khan',
-    text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
-},
-{
-    id:2,
-    src: '/zeeshan.jfif',
-    heading:'Zeeshan Hanif',
-    text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
-},
-{
-    id:3,
-    src: '/daniyal.jfif',
-    heading:'Daniyal Nagori',
-    text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
-},
-{
-    id:4,
-    src: '/hira.jpg',
-    heading:'Hira Khan',
-    text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
-},
-{
-    id:5,
-    src: '/Adil.jfif',
-    heading:'Adil Altaf',
-    text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
-}
+export const data = [
+    {
+        id: 1,
+        src: '/Ziasir.webp',
+        heading: 'Zia Khan',
+        text: 'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 2,
+        src: '/zeeshan.jfif',
+        heading: 'Zeeshan Hanif',
+        text: 'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 3,
+        src: '/daniyal.jfif',
+        heading: 'Daniyal Nagori',
+        text: 'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 4,
+        src: '/hira.jpg',
+        heading: 'Hira Khan',
+        text: 'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    },
+    {
+        id: 5,
+        src: '/Adil.jfif',
+        heading: 'Adil Altaf',
+        text: 'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
+    }
 ]
 
 //array was made and passed as prop. properties were passed
 //database file was imported, data is main constant, all objects to print here from database and .map function was used here. prop(ins) will carry all data. e.g. ins.image,ins.src,ins.heading, ins.text.
+
+
+export const cards = [
+    {
+        id: 1,
+        Heading1: '1- Web 3.0 (Blockchain) and Metaverse Specialization',
+        Text1: 'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.',
+        Heading2: 'Quarter IV',
+        Text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        Heading3: 'Quarter V',
+        Text3: 'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences',
+    },
+    {
+        id: 2,
+        Heading1: '2- Artificial Intelligence (AI) and Deep Learning Specialization',
+        Text1: 'The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.',
+        Heading2: 'Quarter IV',
+        Text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        Heading3: 'Quarter V',
+        Text3: 'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences',
+    },
+    {
+        id: 3,
+        Heading1: '3- Cloud-Native Computing Specialization',
+        Text1: 'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.',
+        Heading2: 'Quarter IV',
+        Text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        Heading3: 'Quarter V',
+        Text3: 'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences',
+    },
+    {
+        id: 4,
+        Heading1: '4- Ambient Computing and IoT Specialization',
+        Text1: 'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.',
+        Heading2: 'Quarter IV',
+        Text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        Heading3: 'Quarter V',
+        Text3: 'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences',
+    },
+    {
+        id: 5,
+        Heading1: '5- Genomics and Bioinformatics Specialization',
+        Text1: 'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.',
+        Heading2: 'Quarter IV',
+        Text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        Heading3: 'Quarter V',
+        Text3: 'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences',
+    },
+    {
+        id: 6,
+        Heading1: '6- Network Programmability and Automation Specialization',
+        Text1: 'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.',
+        Heading2: 'Quarter IV',
+        Text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        Heading3: 'Quarter V',
+        Text3: 'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences',
+    },
+]
+
+export const core = [
+    {
+        id:1,
+        heading: 'Quarter I',
+        text:'CS-101: Object-Oriented Programming using TypeScript and Typescript for React',
+
+    },
+    {
+        id:2,
+        heading: 'Quarter II',
+        text:'W2-201: Developing Planet-Scale Web 2.0 Apps and APIs using Next.js 13',
+
+    },
+    {
+        id:3,
+        heading: 'Quarter III',
+        text:'DM-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development',
+
+    },
+]
--- src/app/syllabus/page.tsx
+++ src/app/syllabus/page.tsx
@@ -1,9 +1,55 @@
 'use client'
+import { Box, Center, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import Home from "../components/Home";
+import { cards } from "../components/database";
+import { core } from "../components/database";
 
-export default function page(){
-    return(
-        <Home title='Syllabus' src='https://s35691.pcdn.co/wp-content/uploads/2022/06/chalkboard-step-ladder-progress-picture-id1391820946.jpg'/>
+export default function page() {
+    return (
+        <>
+            <Home title='Syllabus' src='https://s35691.pcdn.co/wp-content/uploads/2022/06/chalkboard-step-ladder-progress-picture-id1391820946.jpg' />
+            <Box pt='30px'>
+                <Container maxW={1400}>
+                    <Center flexDir='column'>
+                        <Heading size='2xl'>Course Syllabus</Heading>
+                        <Text fontWeight='medium' mt='20px' textAlign='center' px='130px'>The first three quarters are shared by all specialties and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a fifteen-month-long hybrid program that includes both onsite and online classes and is divided into five quarters of 13 weeks each</Text>
+                    </Center>
+                    <Center flexDir='column' mt='20px'>
+                        <Heading>Common In All</Heading>
+                        <Text fontWeight='medium' mt='20px' textAlign='center' px='130px'>Every participant of the program will start by completing the following three core courses:</Text>
+                    </Center>
+                    <Center>
+                        <Flex mt='30px' gap='20px'>
+                            {core.map((elem) => (
+                                <Box textAlign='center' p='30px' boxShadow='dark-lg' borderRadius='30px' width='290px' key={elem.id}><Heading>{elem.heading}</Heading><Text fontWeight='medium' pt='5px'>{elem.text}</Text></Box>
+                            ))}
+                        </Flex>
+                    </Center>
+                    <Center flexDir='column' mt='20px'>
+                        <Heading>Specialized Tracks</Heading>
+                        <Text fontWeight='medium' mt='20px' textAlign='center' px='40px'>After completing the first two quarters the participants will select one or more specializations consisting of two courses each:</Text>
+                    </Center>
+                </Container>
+
+                <Container maxW={1400} mt='50px' bgSize='cover' bgAttachment='fixed' bgImage='https://www.intechnic.com/hubfs/Blog/Featured%20Images/Color%20Accessibility%20%3F%20UX%20Best%20Practices%20for%20Using%20Color%20in%20Design.jpg'>
+                    {cards.map((elem) => (
+                        <Box pt='40px' key={elem.id}>
+                            <Center flexDir='column' mt='50px'>
+                                <Heading textColor='black'>{elem.Heading1}</Heading>
+                                <Text fontWeight='medium' textColor='black' mt='20px' textAlign='center' px='130px'>{elem.Text1}</Text>
+                            </Center>
+                            <Center>
+                                <Flex mt='30px' gap='20px'>
+                                    <Box bgColor='white' textColor='black' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='30px' width='350px'><Heading>{elem.Heading2}</Heading><Text fontWeight='medium' pt='5px'>{elem.Text2}</Text></Box>
+                                    <Box bgColor='white' textColor='black' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='30px' width='350px'><Heading>{elem.Heading3}</Heading><Text fontWeight='medium' pt='5px'>{elem.Text3}</Text></Box>
+                                </Flex>
+                            </Center>
+                        </Box>
+                    ))}
+                </Container>
+            </Box>
+
+        </>
     )
 }
